c4ab0d9e2aaf4889d34dbed335140f1658d93516
OYKU <OYKU@LAPTOP-F58VDQS5>
2022-10-03 19:18:09 +0300

    commit2

--- gitornek1/src/gitornek1/MainClass.java
+++ gitornek1/src/gitornek1/MainClass.java
@@ -2,7 +2,8 @@ package gitornek1;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.err.println("öykü");	
+		System.err.println("öykü");
+		System.err.println("öykü arslan");
 	}
 	
 
